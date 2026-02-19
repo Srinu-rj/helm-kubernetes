66d4cd8440aaf6a8839f9f1f150d5a719f3a80b0
Srinu-rj <dnsrinu143@gmail.com>
2026-02-19 18:30:35 +0530

    Helm k8s v5.0

--- private-aks/terraform/argocd.tf
+++ private-aks/terraform/argocd.tf
@@ -1,18 +1,18 @@
-resource "helm_release" "argocd" {
-  name       = "argocd"
-  repository = "https://argoproj.github.io/argo-helm"
-  chart      = "argo-cd"
-  namespace  = "argocd"
-  create_namespace = true
-  version    = "6.7.11"  # Use a stable version
-  # values = [file("values.yaml")]
-  values = [<<EOF
-server:
-  service:
-    type: LoadBalancer
-EOF
-  ]
-
-
-}
-
+# resource "helm_release" "argocd" {
+#   name       = "argocd"
+#   repository = "https://argoproj.github.io/argo-helm"
+#   chart      = "argo-cd"
+#   namespace  = "argocd"
+#   create_namespace = true
+#   version    = "6.7.11"  # Use a stable version
+#   # values = [file("values.yaml")]
+#   values = [<<EOF
+# server:
+#   service:
+#     type: LoadBalancer
+# EOF
+#   ]
+#
+#
+# }
+#
